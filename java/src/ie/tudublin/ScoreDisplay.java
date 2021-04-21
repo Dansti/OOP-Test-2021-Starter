d08b5f0ab37171986de7be298f535b59a7640c24
Dansti <danstibayev@gmail.com>
2021-04-21 20:19:16 +0100

    Displaying color

--- java/src/ie/tudublin/ScoreDisplay.java
+++ java/src/ie/tudublin/ScoreDisplay.java
@@ -44,18 +44,26 @@ public class ScoreDisplay extends PApplet
 
 		for(int i=0;i<score.length();i++)
 		{
+			colorMode(RGB);
 			char check = score.charAt(i);
 			if(Character.isLetter(check))
 			{
 				j++;
+				if(mouseX>width/21+width/21*j && mouseY>height-height/8-10*j)
+				{
+					fill(255,0,0);
+				}
+				else
+				{
+					fill(0,0,0);
+				}
 				text(check,width/12+50*j,height/2-100);
-				line(width-width/12-width/21*j+10,height/2+10*j,width-width/12-width/21*j+10,height/2+10*j-height/2+200);
-				ellipse(width-width/12-width/21*j,height/2+10*j,25,25);
+				line(width/21+width/21*j+10,height-height/8-10*j,width/21+width/21*j+10,height-height/8-10*j-60);
+				ellipse(width/21+width/21*j,height-height/8-10*j,25,25);
 			}
 			else
 			{
-				int testing =width-width/12-width/21*j+10;
-				line(testing,height/2+10*j-height/2+200,width-width/12-width/21*j+30,(height/2+10*j-height/2+200)+20);
+				line(width/21+width/21*j+10,height-height/8-10*j-60,width/21+width/21*j+20,(height-height/8-10*j-60)+30);
 			}
 		}
 	}
@@ -87,7 +95,7 @@ public class ScoreDisplay extends PApplet
 
 	public void printScores()
 	{
-		System.out.print(notes);
+		notes.toString();
 	}
 
 	public class Note
@@ -112,7 +120,7 @@ public class ScoreDisplay extends PApplet
 			{
 				qc = "Crotchet";
 			}
-			return "\n" + note + " " + duration + " " + qc + " " + "\n";
+			return note + duration + qc;
 		}
 	}
 
